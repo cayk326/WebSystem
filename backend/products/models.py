7893b1a4eebc392f007268a395e87a78ca007018
cayk326 <yuta0326mj@gmail.com>
2023-09-27 17:34:40 +0900

    Fix issue of setting regarding database. Add finished_date

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model):
@@ -6,7 +7,7 @@ class Post(models.Model):
     author = models.CharField('作者', max_length=50)
     image = models.ImageField(upload_to='images', verbose_name='イメージ画像')
     content = models.TextField('本文')
-    finished_date = models.DateField('読み終わった日', auto_now_add=True)
+    finished_date = models.DateField(verbose_name="読み終わった日", blank=False, null=False, default=timezone.now) #期日
     created_at = models.DateTimeField('作成日', auto_now_add=True)
     updated_at = models.DateField('更新日', auto_now=True)
 
