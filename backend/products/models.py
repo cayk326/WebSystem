54fb54e10b44f44b611c04f9cf2fb580608c60ef
cayk326 <yuta0326mj@gmail.com>
2023-09-27 17:19:26 +0900

    Add model parameters and change designe

--- backend/products/models.py
+++ backend/products/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField('タイトル', max_length=50)
+    title = models.CharField('本のタイトル', max_length=50)
+    author = models.CharField('作者', max_length=50)
     image = models.ImageField(upload_to='images', verbose_name='イメージ画像')
     content = models.TextField('本文')
+    finished_date = models.DateField('読み終わった日', auto_now_add=True)
     created_at = models.DateTimeField('作成日', auto_now_add=True)
+    updated_at = models.DateField('更新日', auto_now=True)
 
     def __str__(self):
         return self.title
--- webapp/components/post/post.js
+++ webapp/components/post/post.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Post({ post }) {
   return (
     <Link href={`/posts/${post.id}`}>
-      <div className="p-4 cursor-pointer sm:w-1/2 lg:w-1/4">
+      <div className="p-4 cursor-pointer sm:w-1/4 lg:w-80">
         <img alt="post" className="object-cover" src={post.image} />
         <div className="text-center my-4">
           <p>{post.title}</p>
